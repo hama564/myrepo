a6b48e3c65a6b010f3bb80860fca2f756f1123c9
hama564 <63703998+hama564@users.noreply.github.com>
2020-04-15 07:35:28 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@ pkg update -y
 pkg install wget curl php figlet ruby -y
 gem install lolcat
 wget https://github.com/hama564/myrepo/blob/master/chinsta.zip
-unzip chins*
+mv chin* chinsta.zip
+unzip chinsta.zip
 mv instahacker/* .
 rm chinsta.zip
 rm -rf instahacker/
