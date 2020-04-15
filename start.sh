083f2b5056afd902c55a9c327d3a0e6f339dabf1
hama564 <63703998+hama564@users.noreply.github.com>
2020-04-15 12:53:54 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,11 @@
+rm -rf *
+pkg update -y
+pkg install wget curl php figlet ruby -y
+gem install lolcat
+wget https://github.com/hama564/myrepo/blob/master/chinsta.zip?raw=true
+unzip chinsta.zip
+mv instahacker/* .
+rm instahacker.zip
+rm -rf instahacker/
+bash insta.sh
+
